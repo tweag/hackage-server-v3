0e7e737b1c39f6ca2fd1c8158682857964f88154
Sandy Maguire <sandy@sandymaguire.me>
2026-07-07 11:49:42 -0700

    Real mirror

---
+++ src/Hackage/Mirror.hs
@@ -0,0 +1,98 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Hackage.Mirror where
+
+import Import
+import Control.Monad (when)
+import Distribution.Pretty qualified as Pretty
+import Data.Coerce
+import GHC.Generics
+import Distribution.Types.PackageId
+import Codec.Archive.Tar qualified as Tar
+import Codec.Archive.Tar.Types qualified as Tar
+import Codec.Compression.Zlib qualified as Zlib
+import Data.ByteString.Lazy qualified as BSL
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as BS
+import Data.Map.Monoidal qualified as MM
+import Data.Map.Monoidal (MonoidalMap)
+import Control.Monad.State
+import Hackage.Types
+import Data.Monoid(Sum(..))
+import Data.Int
+import Data.Text qualified as T
+import Distribution.Types.Version (mkVersion)
+import Distribution.Types.PackageName
+import Rel8 hiding (run)
+import Data.Time.Clock.POSIX
+
+
+
+data RevState = RevState
+  { rs_meta_rev :: Sum Int64
+  , rs_tar_rev :: Sum Int64
+  }
+  deriving stock (Eq, Ord, Show, Generic)
+  deriving (Semigroup, Monoid) via Generically RevState
+
+metaRev :: RevState -> MetadataRevIx
+metaRev = coerce . rs_meta_rev
+
+tarRev :: RevState -> TarballRevIx
+tarRev = coerce . rs_tar_rev
+
+newMetaRev :: PackageIdentifier -> MetadataRevIx -> Tar.GenEntry BSL.ByteString b c -> Statement ()
+newMetaRev pid rev e = do
+  pkgid <- mkPkgIdentifier pid
+  mkMetadataRev
+    pkgid
+    rev
+    (case Tar.entryContent e of
+       Tar.NormalFile x y -> BS.toStrict x
+       _ -> error "something else"
+    )
+    ( posixSecondsToUTCTime $ fromIntegral $ Tar.entryTime e
+    , UserId $ fromIntegral $ Tar.ownerId $ Tar.entryOwnership e
+    )
+  pure ()
+
+
+data UpdateType = TarballRev | MetadataRev
+  deriving stock (Eq, Ord, Show)
+
+main :: IO ()
+main = do
+  bs <- BSL.readFile "/home/sandy/01-index.tar"
+  let es = Tar.read bs
+  flip evalStateT (mempty @(MonoidalMap PackageIdentifier RevState)) $
+    Tar.foldEntries (\e m -> do
+      let o = Tar.entryOwnership e
+      let ps = T.split (== '/') $ T.pack $ Tar.entryPath e
+      when (ps !! 1 /= "preferred-versions") $ do
+        let pname = ps !! 0
+            pvers = mkVersion $ fmap (read . T.unpack) $ T.split (== '.') $ ps !! 1
+            revtype =
+              case ps !! 2 of
+                "package.json" -> TarballRev
+                _ -> MetadataRev
+            pid = PackageIdentifier (mkPackageName $ T.unpack pname) pvers
+
+        me <- gets $ MM.lookup pid
+        case revtype of
+          MetadataRev -> do
+            -- liftIO $ putStrLn $ Pretty.prettyShow pid
+            liftIO $ putStrLn $ showStatement $ newMetaRev pid (maybe 0 metaRev me) e
+          TarballRev -> pure ()
+
+        modify' $ mappend $ MM.singleton pid $
+          case revtype of
+            TarballRev -> mempty { rs_tar_rev = 1 }
+            MetadataRev -> mempty { rs_meta_rev = 1 }
+        -- liftIO $ putStrLn $ unwords
+        --   [ Tar.entryPath e
+        --   , Tar.ownerName o
+        --   , show $ Tar.ownerId o
+        --   ]
+      m
+                    ) (pure ()) (liftIO . print) es
+
--- src/Import.hs
+++ src/Import.hs
@@ -6,86 +6,114 @@
 module Import where
 
 -- import Distribution.Server.Packages.PackageIndex (PackageIndex(..))
--- import System.FilePath ((</>))
--- import Data.Foldable
--- import Data.Text.Encoding (decodeUtf8)
--- import Data.Function (on)
--- import Hackage.Types.PrimaryKey
+import System.FilePath ((</>))
+import Data.Foldable
+import Data.Text.Encoding (decodeUtf8)
+import Data.Function (on)
+import Hackage.Types.PrimaryKey
 -- import Distribution.Server.Packages.Types hiding (pkgInfoId)
 -- import Distribution.Server.Features.UserDetails.Types qualified as V2
 -- import Distribution.Server.Users.Types qualified as V2
 -- import Distribution.Server.Packages.Types qualified as V2
 -- import Distribution.Server.Framework.BlobStorage qualified as V2
--- -- import Data.TarIndex
--- import Distribution.Package (PackageIdentifier(..))
--- import Distribution.Package qualified as Cabal
--- import Rel8 hiding (run)
--- import qualified Rel8 as Rel8
--- import Rel8.Expr.Time (now)
--- import Hasql.Session (statement, run)
--- import Hackage.Types
--- import Hackage.Schemas.Packages
--- import Hackage.Schemas.Users
--- import Data.Text qualified as T
+-- import Data.TarIndex
+import Distribution.Package (PackageIdentifier(..))
+import Distribution.Package qualified as Cabal
+import Rel8 hiding (run)
+import qualified Rel8 as Rel8
+import Rel8.Expr.Time (now)
+import Hasql.Session (statement, run)
+import Hackage.Types
+import Hackage.Schemas.Packages
+import Hackage.Schemas.Users
+import Data.Text qualified as T
+import Data.ByteString
+import Data.Time (UTCTime)
 
 -- import Data.Acid (openLocalStateFrom, query, closeAcidState)
 -- import qualified Distribution.Server.Users.Users as Users
 -- import Distribution.Server.Users.State (GetUserDb(..))
 -- import Distribution.Server.Features.UserDetails.Acid (GetUserDetailsTable(..), UserDetailsTable(..))
 -- import Distribution.Server.Features.Core.State (initialPackagesState, GetPackagesState(..), PackagesState(..))
--- import Data.IntMap qualified as IM
--- import TestAPI (mkConn)
+import Data.IntMap qualified as IM
+import TestAPI (mkConn)
 
 
 
 
--- noUpsert
---     :: (Projecting names index, _)
---     => Projection names index
---     -> (Transpose Expr names -> Expr a)
---     -> OnConflict names
--- noUpsert idx f = DoUpdate $ Upsert
---   { index = idx
---   , predicate = Nothing
---   , set = const id
---   , updateWhere = on (==.) f
---   }
+noUpsert
+    :: (Projecting names index, _)
+    => Projection names index
+    -> (Transpose Expr names -> Expr a)
+    -> OnConflict names
+noUpsert idx f = DoUpdate $ Upsert
+  { index = idx
+  , predicate = Nothing
+  , set = const id
+  , updateWhere = on (==.) f
+  }
 
 
 
--- mkPkgName :: PackageName -> Statement (Query (Expr PkgId))
--- mkPkgName name = insert $
---   Insert
---     { into = packageNameSchema
---     , rows = values @_ @[]
---         [ PackageNameRow
---             { packageNameId = newPrimaryKey
---             , packageName = lit name
---             }
---         ]
---     , onConflict = noUpsert packageName packageNameId
---     , returning = Returning packageNameId
---     }
+mkPkgName :: PackageName -> Statement (Query (Expr PkgId))
+mkPkgName name = insert $
+  Insert
+    { into = packageNameSchema
+    , rows = values @_ @[]
+        [ PackageNameRow
+            { packageNameId = newPrimaryKey
+            , packageName = lit name
+            }
+        ]
+    , onConflict = noUpsert packageName packageNameId
+    , returning = Returning packageNameId
+    }
 
--- mkPkgIdentifier :: PackageIdentifier -> Statement (Query (Expr PkgInfoId))
--- mkPkgIdentifier pkgid = do
---   pkgname <- mkPkgName $ Cabal.packageName pkgid
---   insert $
---     Insert
---       { into = pkgInfoSchema
---       , rows = do
---           pkgnameid <- pkgname
---           values @_ @[]
---             [ PkgInfoRow
---                 { pkgInfoId = newPrimaryKey
---                 , pkgId = pkgnameid
---                 , packageVersion = lit $ Cabal.packageVersion pkgid
---                 , pkgInfoDeprecated = lit False
---                 }
---             ]
---       , onConflict = noUpsert (liftA2 (,) pkgId packageVersion) pkgInfoId
---       , returning = Returning pkgInfoId
---       }
+mkPkgIdentifier :: PackageIdentifier -> Statement (Query (Expr PkgInfoId))
+mkPkgIdentifier pkgid = do
+  pkgname <- mkPkgName $ Cabal.packageName pkgid
+  insert $
+    Insert
+      { into = pkgInfoSchema
+      , rows = do
+          pkgnameid <- pkgname
+          values @_ @[]
+            [ PkgInfoRow
+                { pkgInfoId = newPrimaryKey
+                , pkgId = pkgnameid
+                , packageVersion = lit $ Cabal.packageVersion pkgid
+                , pkgInfoDeprecated = lit False
+                }
+            ]
+      , onConflict = noUpsert (liftA2 (,) pkgId packageVersion) pkgInfoId
+      , returning = Returning pkgInfoId
+      }
+
+
+mkMetadataRev
+    :: Query (Expr PkgInfoId)
+    -> MetadataRevIx
+    -> ByteString
+    -> (UTCTime, UserId)
+    -> Statement (Query (Expr PkgRevId))
+mkMetadataRev qpkgid (revix) cabal (time, uid) = do
+  insert $ Insert
+    { into = metadataRevisionsSchema
+    , rows = do
+        pkgid <- qpkgid
+        values @_ @[]
+          [ MetadataRevisionRow
+              { metadataId = newPrimaryKey
+              , metadataPkgId = pkgid
+              , metadataRevId = lit $ fromIntegral revix
+              , metadataTime = lit time
+              , metadataUploader = lit uid
+              , metadataCabalFile = lit cabal
+              }
+          ]
+    , onConflict = DoNothing
+    , returning = Returning metadataId
+    }
 
 
 -- mkMetadataRev
