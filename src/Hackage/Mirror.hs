cd321d39452737bab0e57f4500f35b3a7d607168
Sandy Maguire <sandy.maguire@tweag.io>
2026-07-16 11:25:49 -0700

    Backfill from acidstate pkgdb

--- src/Hackage/Mirror.hs
+++ src/Hackage/Mirror.hs
@@ -4,11 +4,12 @@ module Hackage.Mirror where
 
 import Codec.Archive.Tar qualified as Tar
 import Codec.Archive.Tar.Entry qualified as Tar
-import Control.Monad (when, void)
+import Control.Monad (when)
 import Control.Monad.State
 import Data.ByteString qualified as BS
 import Data.ByteString.Lazy qualified as BSL
 import Data.Coerce
+import Data.Foldable
 import Data.Int
 import Data.Map.Monoidal (MonoidalMap)
 import Data.Map.Monoidal qualified as MM
@@ -19,13 +20,18 @@ import Distribution.Types.PackageId
 import Distribution.Types.PackageName
 import Distribution.Types.Version (mkVersion)
 import GHC.Generics
-import Hackage.Schemas.Packages (PkgRevId, TarIndexId, TarIndexRow(..), tarIndexSchema)
+import Hackage.Schemas.Packages (PkgRevId)
 import Hackage.Types
 import Hasql.Session (statement, run)
 import Import
 import Rel8 hiding (run)
 import Rel8 qualified as Rel8
 import TestAPI (mkConn)
+
+import Data.Acid (openLocalStateFrom, query, closeAcidState)
+import Distribution.Server.Features.Core.State (initialPackagesState, GetPackagesState(..), PackagesState(..))
+import Distribution.Server.Packages.PackageIndex (PackageIndex(..))
+import System.FilePath
 
 
 
@@ -66,13 +72,13 @@ newMetaRev pid rev e = do
 data UpdateType = TarballRev | MetadataRev
   deriving stock (Eq, Ord, Show)
 
-main :: IO ()
-main = mkConn $ \conn -> do
+importIndexTar :: IO ()
+importIndexTar = mkConn $ \conn -> do
   bs <- BSL.readFile "/home/sandy/01-index.tar"
   let es = Tar.read bs
   flip evalStateT (mempty @(MonoidalMap PackageIdentifier RevState)) $
     Tar.foldEntries (\e m -> do
-      let o = Tar.entryOwnership e
+      -- let o = Tar.entryOwnership e
       let ps = T.split (== '/') $ T.pack $ Tar.entryPath e
       when (ps !! 1 /= "preferred-versions") $ do
         let pname = ps !! 0
@@ -88,7 +94,7 @@ main = mkConn $ \conn -> do
           MetadataRev -> do
             -- liftIO $ putStrLn $ Pretty.prettyShow pid
             (either (error . show) (const $ pure ()) =<<) $ liftIO $ flip run conn $ statement () $ Rel8.run $ runSqlM $ do
-              void $ mkUser (UserId $ fromIntegral $ Tar.ownerId o) (T.pack $ Tar.ownerName o)
+              -- _ <- mkUser (UserId $ fromIntegral $ Tar.ownerId o) (T.pack $ Tar.ownerName o)
               newMetaRev pid (maybe 0 metaRev me) e
           TarballRev -> pure ()
 
@@ -97,4 +103,23 @@ main = mkConn $ \conn -> do
             TarballRev -> mempty { rs_tar_rev = 1 }
             MetadataRev -> mempty { rs_meta_rev = 1 }
       m) (pure ()) (liftIO . print) es
+
+
+backfillPackageDB :: IO ()
+backfillPackageDB = do
+  let dbDir = ".." </> "hackage-server" </> "state" </> "db"
+
+  packagesH <- openLocalStateFrom (dbDir </> "PackagesState") (initialPackagesState False)
+
+  PackagesState (PackageIndex pkgs) _ <- query packagesH GetPackagesState
+
+  closeAcidState packagesH
+
+  mkConn $ \conn -> do
+    flip evalStateT (mempty @(MonoidalMap PackageIdentifier RevState)) $
+      (either (error . show) (const $ pure ()) =<<) $ liftIO $
+        flip run conn $ statement () $
+          Rel8.run $ runSqlM $ do
+            for_ pkgs $ traverse insertPkgInfo
+            pure $ pure $ lit True
 
--- src/Import.hs
+++ src/Import.hs
@@ -6,12 +6,13 @@
 
 module Import where
 
+import Data.Foldable
 import Codec.Archive.Tar qualified as Tar
 import Codec.Archive.Tar.Entry qualified as Tar
 import Control.Lens (Lens', at, view, (&), (.~))
 import Control.Monad.Accum
 import Control.Monad.State
-import Data.ByteString
+import Data.ByteString (ByteString)
 import Data.ByteString.Lazy qualified as BSL
 import Data.Generics.Labels ()
 import Data.Int (Int64)
@@ -30,18 +31,9 @@ import Rel8 hiding (run)
 import Rel8.Expr.Time (now)
 import Servant.Tarball
 
--- import Data.Acid (openLocalStateFrom, query, closeAcidState)
--- import Data.TarIndex
--- import Distribution.Server.Features.Core.State (initialPackagesState, GetPackagesState(..), PackagesState(..))
--- import Distribution.Server.Features.UserDetails.Acid (GetUserDetailsTable(..), UserDetailsTable(..))
--- import Distribution.Server.Features.UserDetails.Types qualified as V2
--- import Distribution.Server.Framework.BlobStorage qualified as V2
--- import Distribution.Server.Packages.PackageIndex (PackageIndex(..))
--- import Distribution.Server.Packages.Types hiding (pkgInfoId)
--- import Distribution.Server.Packages.Types qualified as V2
--- import Distribution.Server.Users.State (GetUserDb(..))
--- import Distribution.Server.Users.Types qualified as V2
--- import qualified Distribution.Server.Users.Users as Users
+import Distribution.Server.Framework.BlobStorage qualified as V2
+import Distribution.Server.Users.Types qualified as V2
+import Distribution.Server.Packages.Types qualified as V2
 
 
 -- | A mapping from Haskell types to queries that cache the results of their
@@ -219,66 +211,37 @@ indexTarEntry bid o e = do
       }
 
 
---------------------------------------------------------------------------------
--- Disabled acid-state importing
-
--- mkMetadataRev
---     :: Query (Expr PkgInfoId)
---     -> V2.MetadataRevIx
---     -> CabalFileText
---     -> OldUploadInfo
---     -> Statement (Query (Expr PkgRevId))
--- mkMetadataRev qpkgid (V2.MetadataRevIx revix) (CabalFileText cabal) (time, V2.UserId uid) = do
---   insert $ Insert
---     { into = metadataRevisionsSchema
---     , rows = do
---         pkgid <- qpkgid
---         values @_ @[]
---           [ MetadataRevisionRow
---               { metadataId = newPrimaryKey
---               , metadataPkgId = pkgid
---               , metadataRevId = lit $ fromIntegral revix
---               , metadataTime = lit time
---               , metadataUploader = lit $ UserId $ fromIntegral uid
---               , metadataCabalFile = lit $ decodeUtf8 cabal
---               }
---           ]
---     , onConflict = DoNothing
---     , returning = Returning metadataId
---     }
-
-
--- mkTarballRev
---   :: Query (Expr PkgInfoId)
---   -> V2.TarballRevIx
---   -> PkgTarball
---   -> OldUploadInfo
---   -> Statement (Query (Expr TarballRevId))
--- mkTarballRev qpkgid (V2.TarballRevIx revix) (PkgTarball (BlobInfo gz len sha) nogz) (time, V2.UserId uid) =
---   insert $ Insert
---     { into = packageTarballRevisionsSchema
---     , rows = do
---         pkgid <- qpkgid
---         values @_ @[]
---           [ TarballRevisionRow
---               { tarballRevId = newPrimaryKey
---               , tarballPkgId = pkgid
---               , tarballRevIx = lit $ fromIntegral revix
---               , tarballTime = lit time
---               , tarballUploader = lit $ UserId $ fromIntegral uid
---               , tarballBlobGz
---                   = lit $ either error BlobId $ parseMD5 $ V2.blobMd5 gz
---               , tarballBlobNoGz
---                   = lit $ either error BlobId $ parseMD5 $ V2.blobMd5 nogz
---               , tarballGzLength = lit $ fromIntegral len
---               , -- TODO(sandy): fixme
---                 tarballGzHash = lit $ T.pack $ show sha
---               }
---           ]
---     , onConflict = DoNothing
---     , returning = Returning tarballRevId
---     }
--- mkTarballRev _ (V2.TarballRevIx _) e _ = error $ show e
+mkTarballRev
+  :: Query (Expr PkgInfoId)
+  -> V2.TarballRevIx
+  -> V2.PkgTarball
+  -> (UTCTime, V2.UserId)
+  -> SqlM (Query (Expr TarballRevId))
+mkTarballRev qpkgid (V2.TarballRevIx revix) (V2.PkgTarball (V2.BlobInfo gz len sha) nogz) (time, V2.UserId uid) =
+  sql $ insert $ Insert
+    { into = packageTarballRevisionsSchema
+    , rows = do
+        pkgid <- qpkgid
+        values @_ @[]
+          [ TarballRevisionRow
+              { tarballRevId = newPrimaryKey
+              , tarballPkgId = pkgid
+              , tarballRevIx = lit $ fromIntegral revix
+              , tarballTime = lit time
+              , tarballUploader = lit $ UserId $ fromIntegral uid
+              , tarballBlobGz
+                  = lit $ either error BlobId $ parseMD5 $ V2.blobMd5 gz
+              , tarballBlobNoGz
+                  = lit $ either error BlobId $ parseMD5 $ V2.blobMd5 nogz
+              , tarballGzLength = lit $ fromIntegral len
+              , -- TODO(sandy): fixme
+                tarballGzHash = lit $ T.pack $ show sha
+              }
+          ]
+    , onConflict = DoNothing
+    , returning = Returning tarballRevId
+    }
+mkTarballRev _ (V2.TarballRevIx _) e _ = error $ show e
 
 -- mkUser
 --   :: V2.UserId
@@ -303,37 +266,12 @@ indexTarEntry bid o e = do
 --   , returning = Returning userId
 --   }
 
--- main :: IO ()
--- main = do
---   let dbDir = ".." </> "hackage-server" </> "state" </> "db"
 
---   usersH    <- openLocalStateFrom (dbDir </> "Users") Users.emptyUsers
---   detailsH  <- openLocalStateFrom (dbDir </> "UserDetails") (UserDetailsTable mempty)
---   packagesH <- openLocalStateFrom (dbDir </> "PackagesState") (initialPackagesState False)
-
---   Users.Users users _ _ _ <- query usersH GetUserDb
---   UserDetailsTable details <- query detailsH GetUserDetailsTable
---   PackagesState (PackageIndex pkgs) _ <- query packagesH GetPackagesState
-
---   closeAcidState usersH
---   closeAcidState detailsH
---   closeAcidState packagesH
-
---   let z = IM.mergeWithKey (const $ \x y -> Just (x, Just y)) (fmap (, Nothing)) (error "hopefully impossible") (fmap V2.userName users) details
---   mkConn $ \conn -> do
---     res <- flip run conn $ statement () $ Rel8.run $ do
---       for_ (IM.toList z) $ \(uid, (uname, deets)) ->
---         mkUser (V2.UserId uid) uname deets
---       for_ pkgs $ traverse_ insertPkgInfo
---       pure $ pure $ lit True
---     print res
---   pure ()
-
--- insertPkgInfo :: PkgInfo -> Statement ()
--- insertPkgInfo (PkgInfo pkgid mdrevs tbrevs) = do
---   epkgid <- mkPkgIdentifier pkgid
---   for_ (zip (toList mdrevs) [0..]) $ \((cabal, oui), revix) ->
---     mkMetadataRev epkgid (V2.MetadataRevIx revix) cabal oui
---   for_ (zip (toList tbrevs) [0..]) $ \((pkgtb, oui), revix) ->
---     mkTarballRev epkgid (V2.TarballRevIx revix) pkgtb oui
+insertPkgInfo :: V2.PkgInfo -> SqlM ()
+insertPkgInfo (V2.PkgInfo pkgid mdrevs tbrevs) = do
+  epkgid <- mkPkgIdentifier pkgid
+  for_ (zip (toList mdrevs) [0..]) $ \((V2.CabalFileText cabal, (a, V2.UserId b)), revix) ->
+    mkMetadataRev epkgid revix cabal (a, UserId $ fromIntegral b)
+  for_ (zip (toList tbrevs) [0..]) $ \((pkgtb, (a, b)), revix) ->
+    mkTarballRev epkgid (V2.TarballRevIx revix) pkgtb (a, b)
 
