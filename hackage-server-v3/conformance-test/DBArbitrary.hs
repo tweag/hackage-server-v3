fe07be12fcfa1267d9c81f8884674bd36b1eef1e
Sandy Maguire <sandy.maguire@tweag.io>
2026-08-20 08:48:09 -0700

    Add HTML conformance tests

--- hackage-server-v3/conformance-test/DBArbitrary.hs
+++ hackage-server-v3/conformance-test/DBArbitrary.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DerivingStrategies         #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE ViewPatterns               #-}
 
 module Main where
 
@@ -11,6 +12,9 @@ import Control.Monad.Reader (ReaderT(..))
 import Data.Aeson (Value (..), decode)
 import Data.BlobStorage qualified as Blob
 import Data.ByteString.Lazy (ByteString)
+import Data.ByteString.Lazy.Char8 qualified as BS8
+import Data.Char (isSpace)
+import Data.List (sort)
 import Data.Pool
 import Data.Proxy (Proxy(..))
 import Data.String (fromString)
@@ -21,7 +25,7 @@ import Hackage.Main hiding (main)
 import Hackage.Objects
 import Hackage.Schemas.Packages
 import Hackage.ServerM
-import Hackage.Types
+import Hackage.Types hiding (Tag)
 import Hackage.Utils
 import Hasql.Connection.Setting qualified as DB
 import Hasql.Connection.Setting.Connection qualified as DB
@@ -36,6 +40,9 @@ import System.FilePath ((</>))
 import System.Random (randomRIO, randomIO)
 import Test.Hspec
 import Test.Hspec.Wai
+import Text.HTML.TagSoup
+import Text.HTML.TagSoup.Match
+import Text.HTML.TagSoup.Tree
 
 
 -- | A monadic generator for random values drawn from the database.
@@ -111,6 +118,15 @@ testOptions = Options
   }
 
 
+-- | Given two lists, find the tails that don't match.
+unmatching :: Eq a => [a] -> [a] -> ([a], [a])
+unmatching [] ys = ([], ys)
+unmatching (x : xs) (y : ys)
+  | x == y = unmatching xs ys
+  | otherwise = (x : xs, y : ys)
+unmatching (x : xs) [] = (x : xs, [])
+
+
 verify
   :: DBGen Link
   -> WaiSession st ()
@@ -133,62 +149,136 @@ verify mklink = do
                     [ show v2
                     , show v3
                     ]
-              Nothing ->
-                case bsv2 == bsv3 of
-                  True -> Nothing
-                  False -> Just $ unlines
-                    [ show bsv2
-                    , show bsv3
+              Nothing -> do
+                let tagsv2 = simplifyTags $ parseTags bsv2
+                    tagsv3 = simplifyTags $ parseTags bsv3
+                case unmatching tagsv2 tagsv3 of
+                  ([], []) -> Nothing
+                  (utagsv2, utagsv3) -> Just $ unlines
+                    [ show utagsv2
+                    , show utagsv3
                     ]
         )
+
+
+trimBS :: ByteString -> ByteString
+trimBS = BS8.dropWhile isSpace . BS8.dropWhileEnd isSpace
+
+simplifyTags :: [Tag ByteString] -> [Tag ByteString]
+simplifyTags
+  = filter
+      ( \case
+        -- V2 has mangled html sometimes and often forgets its closing tags.
+        TagClose{} -> False
+        -- V2 is inconsistent about its doctypes
+        TagOpen "!DOCTYPE" _ -> False
+        -- V2 is inconsistent about its html attributes
+        TagOpen "html" _ -> False
+        -- V2 is inconsistent about its metas
+        TagOpen "meta" _ -> False
+        -- V2 is inconsistent about its links
+        TagOpen "link" _ -> False
+        -- V2 is inconsistent about its scripts
+        TagOpen "script" _ -> False
+        _ -> True
+      )
+  . fmap
+      ( \case
+        TagOpen a b -> TagOpen a (sort b)
+        x -> x
+      )
+  . flattenTree
+  . transformTree
+      ( \case
+          TagLeaf (TagText s) ->
+            case trimBS s of
+              "" -> mempty
+              s' -> pure $ TagLeaf $ TagText $ BS8.filter (not . isSpace) s'
+          TagBranch "style" _ _ -> mempty
+          TagBranch "ul" (anyAttrLit ("id", "page-menu") -> True) _ -> mempty
+          x -> pure x
+      )
+  . tagTree
+  . canonicalizeTags
 
 
 spec :: Spec
 spec =
   with (do
       pool <- newPool $ connPool testOptions
-      blobStore <- Blob.open "../hackage-server/state/blobs"
+      blobStore <- Blob.open "../../hackage-server/state/blobs"
       serverMToWai
         (Proxy @(NamedRoutes PackageDbApi))
         (UserDomain (optUserDomain testOptions) :. EmptyContext)
         (ServerCtx pool blobStore)
         packageDbServer
       ) $ do
-    it "revisions" $ verify $ do
-      a <- genPackageLocator
-      pure $ fieldLink pkgdb_api_revisions_redirect (Just $ NegotiatedContent "json") a
-    it "revision metadata" $ verify $ do
-      a <- genPackageId
-      rev <- genMetadataRevIx $ Specific a
-      pure $ fieldLink pkgdb_api_revisionMetadata (Just $ NegotiatedContent "json") a rev
-    it "revision cabal" $ verify $ do
-      a <- genPackageId
-      rev <- genMetadataRevIx $ Specific a
-      pure $ fieldLink pkgdb_api_revisionCabal a rev
-    it "tarball" $ verify $ do
-      a <- genPackageId
-      pure $ fieldLink pkgdb_api_tarball (Specific a) a
-    it "uploader" $ verify $ do
-      a <- genPackageLocator
-      pure $ fieldLink pkgdb_api_uploader a
-    it "upload time" $ verify $ do
-      a <- genPackageLocator
-      pure $ fieldLink pkgdb_api_uploadTime a
-    it "versions" $ verify $ do
-      a <- genPackageName
-      pure $ fieldLink pkgdb_api_versions a
-    it "metadata" $ verify $ do
-      a <- genPackageId
-      pure $ fieldLink pkgdb_api_metadata a
-    it "cabalFile" $ verify $ do
-      a <- genPackageLocator
-      pure $ fieldLink pkgdb_api_cabalFile a $ packageLocName a
-    it "preferredVersions" $ verify $ do
-      a <- genPackageName
-      pure $ fieldLink pkgdb_api_preferredVersions (Just $ NegotiatedContent "json") a
-    it "deprecated" $ verify $ do
-      a <- genPackageName
-      pure $ fieldLink pkgdb_api_deprecated (Just $ NegotiatedContent "json") a
+    describe "non-html endpoints" $ do
+      it "revisions" $ verify $ do
+        a <- genPackageLocator
+        pure $ fieldLink pkgdb_api_revisions_redirect (Just $ NegotiatedContent "json") a
+      it "revision metadata" $ verify $ do
+        a <- genPackageId
+        rev <- genMetadataRevIx $ Specific a
+        pure $ fieldLink pkgdb_api_revisionMetadata (Just $ NegotiatedContent "json") a rev
+      it "revision cabal" $ verify $ do
+        a <- genPackageId
+        rev <- genMetadataRevIx $ Specific a
+        pure $ fieldLink pkgdb_api_revisionCabal a rev
+      it "tarball" $ verify $ do
+        a <- genPackageId
+        pure $ fieldLink pkgdb_api_tarball (Specific a) a
+      it "uploader" $ verify $ do
+        a <- genPackageLocator
+        pure $ fieldLink pkgdb_api_uploader a
+      it "upload time" $ verify $ do
+        a <- genPackageLocator
+        pure $ fieldLink pkgdb_api_uploadTime a
+      it "versions" $ verify $ do
+        a <- genPackageName
+        pure $ fieldLink pkgdb_api_versions a
+      it "metadata" $ verify $ do
+        a <- genPackageId
+        pure $ fieldLink pkgdb_api_metadata a
+      it "cabalFile" $ verify $ do
+        a <- genPackageLocator
+        pure $ fieldLink pkgdb_api_cabalFile a $ packageLocName a
+      it "preferredVersions" $ verify $ do
+        a <- genPackageName
+        pure $ fieldLink pkgdb_api_preferredVersions (Just $ NegotiatedContent "json") a
+      it "deprecated" $ verify $ do
+        a <- genPackageName
+        pure $ fieldLink pkgdb_api_deprecated (Just $ NegotiatedContent "json") a
+      it "all deprecated" $ verify $ do
+        pure $ fieldLink pkgdb_api_allDeprecated $ Just $ NegotiatedContent "json"
+      it "docs tarball" $ verify $ do
+        a <- genPackageLocator
+        pure $ fieldLink pkgdb_api_docsTarball a
+      it "changelog" $ verify $ do
+        a <- genPackageLocator
+        pure $ fieldLink pkgdb_api_changelog (Just $ NegotiatedContent "json") a
+
+    describe "html endpoints" $ do
+      it "revisions" $ verify $ do
+        a <- genPackageLocator
+        pure $ fieldLink pkgdb_api_revisions_redirect (Just $ NegotiatedContent "html") a
+      it "distro monitor" $ verify $ do
+        a <- genPackageName
+        pure $ fieldLink pkgdb_api_distroMonitor Nothing a
+      it "dependencies" $ verify $ do
+        a <- genPackageLocator
+        pure $ fieldLink pkgdb_api_dependencies a
+      it "preferredVersions" $ verify $ do
+        a <- genPackageName
+        pure $ fieldLink pkgdb_api_preferredVersions (Just $ NegotiatedContent "html") a
+      it "deprecated" $ verify $ do
+        a <- genPackageName
+        pure $ fieldLink pkgdb_api_deprecated (Just $ NegotiatedContent "html") a
+      it "all deprecated" $ verify $ do
+        pure $ fieldLink pkgdb_api_allDeprecated $ Just $ NegotiatedContent "html"
+      it "changelog" $ verify $ do
+        a <- genPackageLocator
+        pure $ fieldLink pkgdb_api_changelog (Just $ NegotiatedContent "html") a
 
 
 main :: IO ()
